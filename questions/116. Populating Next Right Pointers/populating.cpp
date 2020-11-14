4846007a47a5f579b7d8e1fb278267237546474d
Xiaoxu Shen <shenxiaoxu22@gmail.com>
2020-11-13 17:23:12 -0800

    Create populating.cpp

---
+++ questions/116. Populating Next Right Pointers/populating.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root) return NULL;
+        Node * pre = root;
+        Node * cur = NULL;
+        while(pre->left){
+            cur = pre;
+            while(cur){
+                cur->left->next = cur->right;
+                if(cur->next) cur->right->next = cur->next->left;
+                cur = cur->next;
+            }
+            pre = pre->left;
+        }
+        return root;
+    }
+};
