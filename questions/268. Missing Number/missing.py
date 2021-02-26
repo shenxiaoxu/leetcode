da88d0d6e52d92419486e6da8c1d9dd909f06fb3
Xiaoxu Shen <shenxiaoxu22@gmail.com>
2021-02-25 21:56:09 -0800

    Create missing.py

---
+++ questions/268. Missing Number/missing.py
@@ -0,0 +1,16 @@
+'''
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+'''
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        xor = 0
+        for i in range(len(nums)):
+            xor = xor ^ i ^ nums[i]
+        
+        return xor^(i+1)
